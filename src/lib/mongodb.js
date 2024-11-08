9d247b0bd8882a637ea0ba78081f170c3973fa79
sancheznot <sancheznotp@gmail.com>
2024-11-08 01:07:28 -0800

    improve the database connetion

--- src/lib/mongodb.js
+++ src/lib/mongodb.js
@@ -8,16 +8,73 @@ if (!MONGODB_URI) {
   );
 }
 
-export const connectMongoDB = async () => {
- try{
-  const { connection } = await mongoose.connect(MONGODB_URI);
-  if (connection.readyState === 1) {
-    console.log("MongoDB connected");
-    return Promise.resolve(true);
-  }
- }catch(e){
-   console.log(e)
-  return Promise.reject(false);
- }
+let cached = global.mongoose;
 
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+export const connectMongoDB = async () => {
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    cached.promise = mongoose
+      .connect(MONGODB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        serverSelectionTimeoutMS: 5000, // Increase the time it takes to detect when the MongoDB server is down
+        socketTimeoutMS: 45000, // Increase the time it takes for the MongoDB server to close the connection
+        connectTimeoutMS: 30000, // Increase the time it takes to connect to the MongoDB server
+      })
+      .then((mongoose) => {
+        return mongoose;
+      });
+  }
+
+  cached.conn = await cached.promise;
+  return cached.conn;
 };
+
+// IMPORTANT:  Don't forget use this middleware in your api routes
+//  Middleware for database connection use it in api routes
+// 
+// const databaseMiddleware = (handler) => async (req, res) => {
+//   if (!global.mongoose) {
+//     global.mongoose = await connectMongoDB();
+//   }
+//   return handler(req, res);
+// };
+
+// Example of using the databaseMiddleware in an API route
+// POST method
+// export const POST = databaseMiddleware(async (req) => {
+//   try {
+
+//     const { data } = await req.json();
+//     const {  userID } = data;
+
+//     if (!userID ) {
+//       return NextResponse.json(
+//         { message: "Bad Request" },
+//         { status: 400 }
+//       );
+//     }
+
+//     const db = await connectMongoDB();
+//     if (!db) {
+//       return NextResponse.json(
+//         { message: "Failed to connect to the database" },
+//         { status: 500 }
+//       );
+//     }
+
+//   } catch (error) {
+//     console.error('Database connection error:', error);
+//     return NextResponse.json(
+//       { message: "Internal Server Error" },
+//       { status: 500 }
+//     );
+//   }
+// });
