b11f7e34b6f10fc04dac7767d1230e645e9989e4
sancheznot <sancheznotp@gmail.com>
2024-11-11 16:21:57 -0800

    add button to 2fa page

--- src/app/(User)/dashboard/page.js
+++ src/app/(User)/dashboard/page.js
@@ -1,17 +1,19 @@
 import { redirect } from "next/navigation";
 import { auth } from "@/auth";
+import { Button } from "@nextui-org/react";
+import Link from "next/link";
 
 export default async function DashboardPage() {
   const session = await auth();
 
-   // If no session, redirect to login
-   if (!session) {
-    redirect("/login")
+  // If no session, redirect to login
+  if (!session) {
+    redirect("/login");
   }
 
   // If 2FA is required, redirect to verification
   if (session.requiresTwoFactor) {
-    redirect("/auth/verify-2fa")
+    redirect("/auth/verify-2fa");
   }
 
   // Ensure we have complete user information
@@ -23,6 +25,15 @@ export default async function DashboardPage() {
     <div>
       <h1>Welcome to Dashboard</h1>
       <pre>{JSON.stringify(session.user, null, 2)}</pre>
+      <Button
+        className="ml-5"
+        color="warning"
+        radius="sm"
+        as={Link}
+        size="sm"
+        href={`/profile/${session.user.email}/setting/2fa`}>
+        Go to 2FA activator page
+      </Button>
     </div>
   );
 }
